0e95acd187d9c151a9988218c081abd79d6c6da2
Shreyansh Tiwari <shreyanshtiwari99@gmail.com>
2022-12-14 20:34:58 +0530

    completed lesson2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Router</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home></Home>}>
+            {" "}
+          </Route>
+          <Route path="about" element={<About></About>}>
+            {" "}
+          </Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
