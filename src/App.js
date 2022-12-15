9a4abe378642dbd6d0c7436e99d21cecd7397329
Shreyansh Tiwari <shreyanshtiwari99@gmail.com>
2022-12-15 15:58:16 +0530

    completed lesson 3

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 //pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+//layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<Home></Home>}>
+        {" "}
+      </Route>
+      <Route path="about" element={<About></About>}>
+        {" "}
+      </Route>
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Router</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home></Home>}>
-            {" "}
-          </Route>
-          <Route path="about" element={<About></About>}>
-            {" "}
-          </Route>
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/layouts/RootLayout.js
@@ -0,0 +1,19 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>Router</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </div>
+  );
+}
